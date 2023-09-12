a76cf0adad58c409fe7060acda1bf21f839d49fd
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-09-12 13:16:02 +0530

    [#6] Milan/Gourab - Add Players entity with methods to get next player and strand player

---
+++ src/models/players.js
@@ -0,0 +1,32 @@
+class Players {
+  #players;
+  #currentPlayerId;
+
+  constructor(players) {
+    this.#players = players;
+    this.#currentPlayerId = 0;
+  }
+
+  getNextPlayer() {
+    const noOfPlayers = this.#players.length;
+    this.#currentPlayerId += 1;
+    const nextPlayer = this.#players[noOfPlayers % this.#currentPlayerId];
+
+    if (nextPlayer.info().isStranded) return this.getNextPlayer();
+    return nextPlayer;
+  }
+
+  findPlayer(playerId) {
+    return this.#players.find(player => player.info().id === playerId);
+  }
+
+  strandPlayer(playerId) {
+    const playerToStrand = this.findPlayer(playerId);
+
+    if (!playerToStrand) return new Error("Invalid Player Id");
+
+    playerToStrand.strand();
+  }
+}
+
+module.exports = Players;
---
+++ test/models/players-test.js
@@ -0,0 +1,52 @@
+const { describe, it } = require("node:test");
+const assert = require("assert");
+const Player = require("../../src/models/player");
+const Players = require("../../src/models/players");
+
+const createPlayers = playerNames => {
+  return playerNames.map((name, id) => {
+    return new Player({ id: id + 1, name, cards: [] });
+  });
+};
+
+describe("Players", () => {
+  describe("getNextPlayer", () => {
+    it("should give the next active player", () => {
+      const [gourab, milan, riya] = createPlayers(["gourab", "milan", "riya"]);
+      const players = new Players([gourab, milan, riya]);
+      const nextPlayer = players.getNextPlayer();
+
+      assert.deepStrictEqual(nextPlayer.info(), gourab.info());
+    });
+
+    it("should give next active player if the next player is stranded", () => {
+      const [gourab, milan, riya] = createPlayers(["gourab", "milan", "riya"]);
+      gourab.strand();
+      const players = new Players([gourab, milan, riya]);
+      const nextPlayer = players.getNextPlayer();
+
+      assert.deepStrictEqual(nextPlayer.info(), milan.info());
+    });
+  });
+
+  describe("strandPlayer", () => {
+    it("should strand the player with the given player id", () => {
+      const [gourab, milan, riya] = createPlayers(["gourab", "milan", "riya"]);
+
+      const players = new Players([gourab, milan, riya]);
+      players.strandPlayer(1);
+      const nextPlayer = players.getNextPlayer();
+
+      assert.deepStrictEqual(nextPlayer.info(), milan.info());
+    });
+
+    it("should give an error when tried to strand player with invalid player id", () => {
+      const [gourab, milan, riya] = createPlayers(["gourab", "milan", "riya"]);
+
+      const players = new Players([gourab, milan, riya]);
+      const result = players.strandPlayer(4);
+
+      assert.ok(result instanceof Error);
+    });
+  });
+});
