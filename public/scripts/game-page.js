26eb276c489b2e345852bd4d6c75b627f191de2d
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-09-14 16:44:38 +0530

    Milan/Sourov revives server after merge conflict at game page.js

--- public/scripts/game-page.js
+++ public/scripts/game-page.js
@@ -9,17 +9,13 @@ const main = () => {
   const bottomPane = document.querySelector("#bottom-pane");
 
   const gameService = new GameService();
-<<<<<<< HEAD
   gameService.getBoardStructure(renderBoard);
-  const view = new View(playersContainer, cardsContainer, generateElement);
-=======
   const view = new View({
     playersContainer,
     cardsContainer,
     bottomPane,
     generateElement
   });
->>>>>>> 6aa3dfa ([#10] Milan/Sourov - Gives an End Turn button to the current player)
   const gameController = new GameController(gameService, view);
 
   gameController.start();
