8c336544689df238398c818399c9642dd96ea1d2
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-09-18 15:20:29 +0530

    [#39] Milan/Raj - Add method in board to all the possible positions based on the step count

--- src/models/board.js
+++ src/models/board.js
@@ -35,18 +35,58 @@ class Board {
     });
   }
 
-  getTileInfo(tileCoordinate, playerPositions = []) {
+  getTileInfo(tileCoordinate, playersPositions = []) {
     const info = {};
 
     info.isPresent = this.#isInside(tileCoordinate);
     if (!info.isPresent) return info;
 
-    info.isBlocked = this.#isBlockedTile(tileCoordinate, playerPositions);
+    info.isBlocked = this.#isBlockedTile(tileCoordinate, playersPositions);
     if (info.isBlocked) return info;
 
     info.isRoomTile = this.#isInsideRoom(tileCoordinate);
 
     return info;
+  }
+
+  #calculateAllPossiblePos(stepCount, currentPosition) {
+    const positions = [];
+    let x = 0;
+    let y = stepCount;
+
+    //eslint-disable-next-line id-length
+    for (let i = 0; i <= stepCount; i++) {
+      //eslint-disable-next-line id-length
+      for (let j = 0; j <= stepCount; j++) {
+        positions.push({
+          x: x + j + currentPosition.x,
+          y: y - j + currentPosition.y
+        });
+      }
+      x--;
+      y--;
+    }
+
+    return positions;
+  }
+
+  #isValidTile(tileCoordinates, playerPositions) {
+    return (
+      this.#isInside(tileCoordinates) &&
+      !this.#isBlockedTile(tileCoordinates, playerPositions) &&
+      !this.#isInsideRoom(tileCoordinates)
+    );
+  }
+
+  getPossibleTiles(stepCount, currentPosition, playersPositions = []) {
+    const possiblePositions = {};
+    this.#calculateAllPossiblePos(stepCount, currentPosition).forEach(pos => {
+      if (this.#isValidTile(pos, playersPositions)) {
+        possiblePositions[`${pos.x},${pos.y}`] = pos;
+      }
+    });
+
+    return possiblePositions;
   }
 }
 
--- test/models/board-test.js
+++ test/models/board-test.js
@@ -1,6 +1,8 @@
 const assert = require("assert");
 const { describe, it } = require("node:test");
 const Board = require("../../src/models/board");
+const rooms = require("../../resources/rooms.json");
+const blockedTiles = require("../../resources/blocked-tiles.json");
 
 describe("Board", () => {
   describe("getTileInfo", () => {
@@ -76,6 +78,49 @@ describe("Board", () => {
         isBlocked: false,
         isRoomTile: true
       });
+    });
+  });
+
+  describe("getPossibleTiles", () => {
+    it("Should give all the possible positions according to number of steps", () => {
+      const board = new Board({
+        dimensions: { length: 24, breadth: 25 },
+        blockedTiles,
+        rooms
+      });
+
+      const pawnPos = { x: 7, y: 5 };
+      const possiblePositions = board.getPossibleTiles(1, pawnPos);
+      const expectedPositions = {
+        "7,6": { x: 7, y: 6 },
+        "7,4": { x: 7, y: 4 },
+        "8,5": { x: 8, y: 5 },
+        "6,5": { x: 6, y: 5 }
+      };
+
+      assert.deepStrictEqual(possiblePositions, expectedPositions);
+    });
+
+    it("Should give all the possible positions according to number of steps", () => {
+      const board = new Board({
+        dimensions: { length: 24, breadth: 25 },
+        blockedTiles,
+        rooms
+      });
+
+      const pawnPos = { x: 7, y: 5 };
+      const possiblePositions = board.getPossibleTiles(2, pawnPos, [pawnPos]);
+      const expectedPositions = {
+        "7,7": { x: 7, y: 7 },
+        "8,6": { x: 8, y: 6 },
+        "6,6": { x: 6, y: 6 },
+        "8,4": { x: 8, y: 4 },
+        "5,5": { x: 5, y: 5 },
+        "6,4": { x: 6, y: 4 },
+        "7,3": { x: 7, y: 3 }
+      };
+
+      assert.deepStrictEqual(possiblePositions, expectedPositions);
     });
   });
 });
